c599f8f21b857b248ad4c624d37577d93727c9a2
SACHIN KUMAR <92384180+s-kumar407@users.noreply.github.com>
2024-03-22 16:28:34 +0530

    Update layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Head from "next/head;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -9,8 +9,13 @@ export const metadata = {
   description: " Discover nearby schools and contribute to our growing database by adding your own school. Find information, reviews, and ratings on educational institutions in your area. Join our community to empower others in making informed decisions about education.",
 };
 
+
+
 export default function RootLayout({ children }) {
   return (
+     <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
